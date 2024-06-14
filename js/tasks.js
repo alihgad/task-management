65444432793f5be01bfa056772822cc31ecebf7f
ali <alihgad2@gmail.com>
2024-06-14 18:37:17 +0300

    donieng

--- js/tasks.js
+++ js/tasks.js
@@ -1,12 +1,18 @@
 
 let task = document.querySelector("#task"),
     description = document.querySelector("#description"),
-    done = document.getElementById("done")
+    done = document.getElementById("done"),
+    loader = `<td colspan="4">
+        <i class="fa-solid fa-search fa-beat-fade fa-3x p-5" ></i >
+                </td > `
+
+                
 
 
-
-
+    document.getElementById("tbody").innerHTML = loader
 async function getData() {
+
+
     let response = await fetch("https://t-mapi-alihgads-projects.vercel.app/tasks")
     let data = await response.json()
     displayTasks(data.result)
@@ -19,17 +25,26 @@ function displayTasks(arr) {
 
 
     let cartona = ""
-    for (let i = 0; i < arr.length; i++) {
-        cartona += `<tr>
-            <td>${arr[i].title}</td>
-            <td>${arr[i].description} </td>
-            <td>${arr[i].status} </td>
-            <td><div class="btn btn-success" onclick = "deleteing(${arr[i].id})">Done <i class="fa-solid fa-check-square fa-xl"></i></div></td>
-          </tr>
-            `
-    }
+    if (arr.length) {
+        for (let i = 0; i < arr.length; i++) {
+            cartona += `<tr>
+                <td>${arr[i].title}</td>
+                <td>${arr[i].description} </td>
+                <td>${arr[i].status} </td>
+                <td>
+                <div class="btn btn-success" onclick = "doneing(${arr[i].id})">Done <i class="fa-solid fa-check-square fa-xl"></i></div>
+                <div class="btn btn-danger" onclick = "deleteing(${arr[i].id})">Delete <i class="fa-solid fa-square-xmark fa-xl"></i></div>
+                </td>
+              </tr>
+                `
+        }
 
-    document.getElementById("tbody").innerHTML = cartona
+        document.getElementById("tbody").innerHTML = cartona
+    }else{
+        document.getElementById("tbody").innerHTML = `<td colspan="4">
+        <h5 class='text-danger'> No Tasks Yet </h5>
+                </td > `
+    }
 
 }
 
@@ -41,6 +56,8 @@ function displayTasks(arr) {
 
 
 async function addTask() {
+    document.getElementById("tbody").innerHTML = loader
+
     if (!task.value) {
         Swal.fire({
             icon: "error",
@@ -68,7 +85,7 @@ async function addTask() {
 
 }
 
-async function api(method, body={},id="") {
+async function api(method, body = {}, id = "") {
     fetch(`https://t-mapi-alihgads-projects.vercel.app/tasks/${id}`, {
         method,
         body: JSON.stringify(body),
@@ -99,10 +116,29 @@ document.getElementById("add").addEventListener("click", function () {
 
 async function deleteing(id) {
 
-    
+    document.getElementById("tbody").innerHTML = loader
+
     console.log(id);
     await fetch(`https://t-mapi-alihgads-projects.vercel.app/tasks/${id}`, {
-        method : "DELETE",
+        method: "DELETE",
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => response.json())
+        .catch(err => console.log(err));
+    ;
+    getData()
+}
+
+
+async function doneing(id) {
+
+    document.getElementById("tbody").innerHTML = loader
+
+    console.log(id);
+    await fetch(`https://t-mapi-alihgads-projects.vercel.app/tasks/${id}`, {
+        method: "PUT",
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
